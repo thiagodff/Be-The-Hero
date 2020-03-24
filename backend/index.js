0e442763b999ff157052795644a375b7244dd6b2
thiagofd <thiago.fdornelles@gmail.com>
2020-03-24 08:31:16 -0300

    backend: config nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({
     evento: "Semana Omnistack 11.0",
-    aluno: "Thiago Fernandes Dornelles"
+    aluno: "Thiago F Dornelles"
   });
 });
 
-app.listen(3334);
+app.listen(3333);
