53dc564e9f0b4306f55259ab10507f00910908ab
thiagofd <thiago.fdornelles@gmail.com>
2020-03-26 11:16:18 -0300

    mobile: incidents page

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
 import Routes from './src/routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+    </>
   );
 }
